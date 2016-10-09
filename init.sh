4ec2b6e251c57e5622ad81d54744fe7767db2aa8
bobrofon <bobrofon@gmail.com>
2016-10-09 16:34:10 +0700

    Add force update

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@ set -eu
 BUILD_TOOL=$("${ANDROID_HOME}/tools/android" list sdk --all | grep 'Android SDK Build-tools, revision 22.0.1' | cut -d '-' -f 1 | sed 's/ //g')
 SDK_PLATFORM=$("${ANDROID_HOME}/tools/android" list sdk --all | grep 'SDK Platform Android 5.1.1, API 22, revision 2' | cut -d '-' -f 1 | sed 's/ //g')
 
-"${ANDROID_HOME}/tools/android" update sdk -u -a -t "${BUILD_TOOL}"
-"${ANDROID_HOME}/tools/android" update sdk -u -a -t "${SDK_PLATFORM}"
+echo yes | "${ANDROID_HOME}/tools/android" update sdk -u -a -t "${BUILD_TOOL}"
+echo yes | "${ANDROID_HOME}/tools/android" update sdk -u -a -t "${SDK_PLATFORM}"
 
-"${ANDROID_HOME}/tools/android" update sdk --no-ui --all --filter extra-android-support
-"${ANDROID_HOME}/tools/android" update sdk --no-ui --all --filter extra-android-m2repository
+echo yes | "${ANDROID_HOME}/tools/android" update sdk --no-ui --all --filter extra-android-support
+echo yes | "${ANDROID_HOME}/tools/android" update sdk --no-ui --all --filter extra-android-m2repository
