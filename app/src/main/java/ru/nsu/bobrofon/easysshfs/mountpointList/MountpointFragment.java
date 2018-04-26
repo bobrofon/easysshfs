3da3e89d8c897266fb5c2d857f2d66a5d841f96a
bobrofon <bobrofon@gmail.com>
2018-04-26 22:12:55 +0700

    fix mount fragment warnings

--- app/src/main/java/ru/nsu/bobrofon/easysshfs/mountpointList/MountpointFragment.java
+++ app/src/main/java/ru/nsu/bobrofon/easysshfs/mountpointList/MountpointFragment.java
@@ -76,8 +76,8 @@ public class MountpointFragment extends Fragment
 		View view = inflater.inflate(R.layout.fragment_mountpoint, container, false);
 
 		// Set the adapter
-		mListView = (AbsListView) view.findViewById(android.R.id.list);
-		((AdapterView<ListAdapter>) mListView).setAdapter(mAdapter);
+		mListView = view.findViewById(android.R.id.list);
+		mListView.setAdapter(mAdapter);
 
 		// Set OnItemClickListener so we can be notified on item clicks
 		mListView.setOnItemClickListener(this);
@@ -118,19 +118,6 @@ public class MountpointFragment extends Fragment
 			// Notify the active callbacks interface (the activity, if the
 			// fragment is attached to one) that an item has been selected.
 			mListener.onFragmentInteraction(position);
-		}
-	}
-
-	/**
-	 * The default content for this Fragment has a TextView that is shown when
-	 * the list is empty. If you would like to change the text, call this method
-	 * to supply the text it should use.
-	 */
-	public void setEmptyText(CharSequence emptyText) {
-		View emptyView = mListView.getEmptyView();
-
-		if (emptyView instanceof TextView) {
-			((TextView) emptyView).setText(emptyText);
 		}
 	}
 
