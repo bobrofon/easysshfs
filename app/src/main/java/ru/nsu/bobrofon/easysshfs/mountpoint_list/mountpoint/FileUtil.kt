766fc8cb001d39ba226ea2f44c3a77f15424890e
Sergey Bobrenok <bobrofon@gmail.com>
2019-06-22 17:43:34 +0700

    Clean file utils

--- app/src/main/java/ru/nsu/bobrofon/easysshfs/mountpoint_list/mountpoint/FileUtil.kt
+++ app/src/main/java/ru/nsu/bobrofon/easysshfs/mountpoint_list/mountpoint/FileUtil.kt
@@ -1,9 +1,8 @@
 package ru.nsu.bobrofon.easysshfs.mountpoint_list.mountpoint
 
 import java.io.File
-import java.lang.reflect.Array
+import java.lang.reflect.Array as JArray
 
-import android.annotation.SuppressLint
 import android.annotation.TargetApi
 import android.content.ContentUris
 import android.content.Context
@@ -15,45 +14,27 @@ import android.os.storage.StorageManager
 import android.provider.DocumentsContract
 import android.provider.MediaStore
 
-@SuppressLint("NewApi")
+// Some strange code from stackoverflow
 object FileUtil {
+    fun getFullPathFromTreeUri(treeUri: Uri, con: Context): String {
+        var (volumeId, documentPath) = splitTreeUri(treeUri)
+        var volumePath = volumeId?.let { getVolumePath(volumeId, con) } ?: return File.separator
 
-    private val PRIMARY_VOLUME_NAME = "primary"
+        volumePath = volumePath.trimEnd(File.separatorChar)
+        documentPath = documentPath.trim(File.separatorChar)
 
-    fun getFullPathFromTreeUri(treeUri: Uri?, con: Context): String? {
-        if (treeUri == null) {
-            return null
-        }
-        var volumePath: String? = FileUtil.getVolumePath(FileUtil.getVolumeIdFromTreeUri(treeUri), con)
-                ?: return File.separator
-        if (volumePath!!.endsWith(File.separator)) {
-            volumePath = volumePath.substring(0, volumePath.length - 1)
-        }
-
-        var documentPath = FileUtil.getDocumentPathFromTreeUri(treeUri)
-        if (documentPath.endsWith(File.separator)) {
-            documentPath = documentPath.substring(0, documentPath.length - 1)
-        }
-
-        return if (documentPath.length > 0) {
-            if (documentPath.startsWith(File.separator)) {
-                volumePath + documentPath
-            } else {
-                volumePath + File.separator + documentPath
-            }
-        } else {
-            volumePath
-        }
+        return if (documentPath.isNotEmpty())
+            volumePath + File.separator + documentPath else volumePath
     }
 
-
-    private fun getVolumePath(volumeId: String?, con: Context): String? {
+    private fun getVolumePath(volumeId: String, context: Context): String? {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
             return null
         }
 
         try {
-            val mStorageManager = con.getSystemService(Context.STORAGE_SERVICE) as StorageManager
+            val mStorageManager =
+                context.getSystemService(Context.STORAGE_SERVICE) as StorageManager
 
             val storageVolumeClazz = Class.forName("android.os.storage.StorageVolume")
 
@@ -63,159 +44,145 @@ object FileUtil {
             val isPrimary = storageVolumeClazz.getMethod("isPrimary")
             val result = getVolumeList.invoke(mStorageManager)
 
-            val length = Array.getLength(result)
+            val length = JArray.getLength(result)
             for (i in 0 until length) {
-                val storageVolumeElement = Array.get(result, i)
+                val storageVolumeElement = JArray.get(result, i)
                 val uuid = getUuid.invoke(storageVolumeElement) as String?
                 val primary = isPrimary.invoke(storageVolumeElement) as Boolean
 
-                // primary volume?
-                if (primary && PRIMARY_VOLUME_NAME == volumeId) {
+                // primary volume
+                if (primary && volumeId == "primary") {
                     return getPath.invoke(storageVolumeElement) as String
                 }
 
-                // other volumes?
-                if (uuid != null) {
-                    if (uuid == volumeId) {
-                        return getPath.invoke(storageVolumeElement) as String
-                    }
+                // other volumes
+                if (uuid == volumeId) {
+                    return getPath.invoke(storageVolumeElement) as String
                 }
             }
 
-            // not found.
+            // not found
             return null
         } catch (ex: Exception) {
             return null
         }
-
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private fun getVolumeIdFromTreeUri(treeUri: Uri): String? {
-        val docId = DocumentsContract.getTreeDocumentId(treeUri)
-        val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+    private fun splitTreeUri(treeUri: Uri): Pair<String?, String> {
+        val treeDocumentId = DocumentsContract.getTreeDocumentId(treeUri)
+        val split = splitId(treeDocumentId)
+        val volumeId = split.getOrNull(0)
+        val path = split.getOrElse(1) { "/" }
 
-        return if (split.size > 0) {
-            split[0]
+        return Pair(volumeId, path)
+    }
+
+    private fun splitId(id: String): Array<String> =
+        id.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+
+    fun getPath(context: Context, uri: Uri): String? =
+        when {
+            isDocumentUri(context, uri) -> getDocumentProviderPath(context, uri)
+            isMediaStoreUri(uri) -> getRemoteAddress(context, uri)
+            isFileUri(uri) -> uri.path
+            else -> null
+        }
+
+    private fun isDocumentUri(context: Context, uri: Uri): Boolean =
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
+                && DocumentsContract.isDocumentUri(context, uri)
+
+    private fun isMediaStoreUri(uri: Uri): Boolean =
+        "content".equals(uri.scheme, ignoreCase = true)
+
+    private fun isFileUri(uri: Uri): Boolean =
+        "file".equals(uri.scheme, ignoreCase = true)
+
+    private fun getDocumentProviderPath(context: Context, uri: Uri): String? = when {
+        isExternalStorageDocument(uri) -> getExternalStorageProviderPath(uri)
+        isDownloadsDocument(uri) -> getDownloadsProviderPath(context, uri)
+        isMediaDocument(uri) -> getMediaProviderPath(context, uri)
+        else -> null
+    }
+
+    private fun getRemoteAddress(context: Context, uri: Uri): String? =
+        if (isGooglePhotosUri(uri)) uri.lastPathSegment else getDataColumn(context, uri)
+
+    private fun isGooglePhotosUri(uri: Uri): Boolean =
+        uri.authority == "com.google.android.apps.photos.content"
+
+    private fun isExternalStorageDocument(uri: Uri): Boolean =
+        uri.authority == "com.android.externalstorage.documents"
+
+    private fun isDownloadsDocument(uri: Uri): Boolean =
+        uri.authority == "com.android.providers.downloads.documents"
+
+    private fun isMediaDocument(uri: Uri): Boolean =
+        uri.authority == "com.android.providers.media.documents"
+
+    private fun getExternalStorageProviderPath(uri: Uri): String {
+        val (uriType, uriValue) = splitUri(uri)
+
+        return if ("primary".equals(uriType, ignoreCase = true)) {
+            Environment.getExternalStorageDirectory().toString() + "/" + uriValue
         } else {
-            null
+            val storageDefinition = if (Environment.isExternalStorageRemovable())
+                "EXTERNAL_STORAGE" else "SECONDARY_STORAGE"
+
+            val storage = System.getenv(storageDefinition) ?: ""
+            "$storage/$uriValue"
         }
     }
 
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private fun getDownloadsProviderPath(context: Context, uri: Uri): String? {
+        val id = DocumentsContract.getDocumentId(uri)
+        val contentUri = ContentUris.withAppendedId(
+            Uri.parse("content://downloads/public_downloads"),
+            java.lang.Long.valueOf(id)
+        )
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private fun getDocumentPathFromTreeUri(treeUri: Uri): String {
-        val docId = DocumentsContract.getTreeDocumentId(treeUri)
-        val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-        return if (split.size >= 2/* && split[1] != null*/) {
-            split[1]
-        } else {
-            File.separator
+        return getDataColumn(context, contentUri)
+    }
+
+    private fun getMediaProviderPath(context: Context, uri: Uri): String? {
+        val (uriType, uriValue) = splitUri(uri)
+        val contentUri: Uri? = when (uriType) {
+            "image" -> MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            "video" -> MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+            "audio" -> MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+            else -> null
         }
+        val selection = "_id=?"
+        val selectionArgs = arrayOf(uriValue)
+
+        return contentUri?.let { getDataColumn(context, contentUri, selection, selectionArgs) }
     }
 
-    fun getPath(context: Context, uri: Uri): String? {
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private fun splitUri(uri: Uri): Pair<String, String> {
+        val documentId = DocumentsContract.getDocumentId(uri)
+        val splitUri = splitId(documentId)
 
-        // DocumentProvider
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && DocumentsContract.isDocumentUri(context, uri)) {
-
-            if (isExternalStorageDocument(uri)) {// ExternalStorageProvider
-                val docId = DocumentsContract.getDocumentId(uri)
-                val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-                val type = split[0]
-                val storageDefinition: String
-
-
-                if ("primary".equals(type, ignoreCase = true)) {
-
-                    return Environment.getExternalStorageDirectory().toString() + "/" + split[1]
-
-                } else {
-
-                    if (Environment.isExternalStorageRemovable()) {
-                        storageDefinition = "EXTERNAL_STORAGE"
-
-                    } else {
-                        storageDefinition = "SECONDARY_STORAGE"
-                    }
-
-                    val storage = System.getenv(storageDefinition) ?: ""
-                    return storage + "/" + split[1]
-                }
-
-            } else if (isDownloadsDocument(uri)) {// DownloadsProvider
-
-                val id = DocumentsContract.getDocumentId(uri)
-                val contentUri = ContentUris.withAppendedId(
-                        Uri.parse("content://downloads/public_downloads"), java.lang.Long.valueOf(id))
-
-                return getDataColumn(context, contentUri, null, null)
-
-            } else if (isMediaDocument(uri)) {// MediaProvider
-                val docId = DocumentsContract.getDocumentId(uri)
-                val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-                val type = split[0]
-
-                var contentUri: Uri? = null
-                if ("image" == type) {
-                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-                } else if ("video" == type) {
-                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
-                } else if ("audio" == type) {
-                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-                }
-
-                val selection = "_id=?"
-                val selectionArgs = arrayOf(split[1])
-
-                return getDataColumn(context, contentUri, selection, selectionArgs)
-            }
-
-        } else if ("content".equals(uri.scheme!!, ignoreCase = true)) {// MediaStore (and general)
-
-            // Return the remote address
-            return if (isGooglePhotosUri(uri)) uri.lastPathSegment else getDataColumn(context, uri, null, null)
-
-        } else if ("file".equals(uri.scheme!!, ignoreCase = true)) {// File
-            return uri.path
-        }
-
-        return null
+        return Pair(splitUri[0], splitUri[1])
     }
 
-    private fun getDataColumn(context: Context, uri: Uri?, selection: String?, selectionArgs: kotlin.Array<String>?): String? {
+    private fun getDataColumn(
+        context: Context, uri: Uri, selection: String? = null,
+        selectionArgs: Array<String>? = null
+    ): String? {
+        val projection = arrayOf(DATA_COLUMN_NAME)
 
-        var cursor: Cursor? = null
-        val column = "_data"
-        val projection = arrayOf(column)
-
-        try {
-            cursor = context.contentResolver.query(uri!!, projection, selection, selectionArgs, null)
-            if (cursor != null && cursor.moveToFirst()) {
-                val column_index = cursor.getColumnIndexOrThrow(column)
-                return cursor.getString(column_index)
-            }
-        } finally {
-            cursor?.close()
-        }
-        return null
+        return context.contentResolver.query(uri, projection, selection, selectionArgs, null)
+            .use { cursor -> cursor?.let { getDataColumn(cursor) } }
     }
 
+    private fun getDataColumn(cursor: Cursor): String? =
+        if (cursor.moveToFirst()) {
+            val columnIndex = cursor.getColumnIndexOrThrow(DATA_COLUMN_NAME)
+            cursor.getString(columnIndex)
+        } else null
 
-    private fun isExternalStorageDocument(uri: Uri): Boolean {
-        return "com.android.externalstorage.documents" == uri.authority
-    }
-
-
-    private fun isDownloadsDocument(uri: Uri): Boolean {
-        return "com.android.providers.downloads.documents" == uri.authority
-    }
-
-    private fun isMediaDocument(uri: Uri): Boolean {
-        return "com.android.providers.media.documents" == uri.authority
-    }
-
-    private fun isGooglePhotosUri(uri: Uri): Boolean {
-        return "com.google.android.apps.photos.content" == uri.authority
-    }
+    private const val DATA_COLUMN_NAME = "_data"
 }
