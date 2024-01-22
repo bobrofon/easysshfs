1bcb85449dcdb761b696678413550f1e1b72359a
Sergey Bobrenok <bobrofon@gmail.com>
2024-01-22 22:39:24 +0300

    Remove tests stab
    It doesn't even compile anymore.

--- app/src/androidTest/kotlin/ru/nsu/bobrofon/easysshfs/ApplicationTest.kt
+++
@@ -1,9 +0,0 @@
-package ru.nsu.bobrofon.easysshfs
-
-import android.app.Application
-import android.test.ApplicationTestCase
-
-/**
- * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
- */
-class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
